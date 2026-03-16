5c6e40618939a62466f9fa5838c63d02dd6c9b71
paritosh1505 <rikkiparitosh43@gmail.com>
2026-03-16 21:57:31 +0530

    Rest basic Crid operation

---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import "net/http"
+
+func main() {
+	http.HandleFunc("/post", createPost)
+	http.HandleFunc("/getPost", getPost)
+	http.HandleFunc("/post/{id}/comment", addComment)
+	http.HandleFunc("/getPost/{id}", fetchPost)
+	http.ListenAndServe(":8080", nil)
+}
---
+++ server.go
@@ -0,0 +1,95 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+	"strconv"
+	"strings"
+)
+
+type postData struct {
+	Id     int    `json:"id"`
+	PostId int    `json:"post_id"`
+	Text   string `json:"text"`
+}
+
+var Allpost = make(map[int][]postData)
+
+func createPost(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		//statsu code -405
+		http.Error(w, "**ONLY POST METHOD ALLOWED**", http.StatusMethodNotAllowed)
+		return
+	}
+	var post postData
+	err := json.NewDecoder(r.Body).Decode(&post)
+	if err != nil {
+		http.Error(w, "Invalid Json", http.StatusBadRequest)
+		return
+	}
+	post.Id += 1
+	if post.PostId == 0 {
+		post.PostId = post.Id
+	}
+	Allpost[len(Allpost)] = append(Allpost[len(Allpost)], post)
+	w.WriteHeader(http.StatusAccepted)
+	json.NewEncoder(w).Encode(Allpost)
+}
+
+func getPost(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodGet {
+		//status code - 405
+		http.Error(w, "**ONLY GET METHOD ALLOWED**", http.StatusMethodNotAllowed)
+		return
+	}
+	json.NewEncoder(w).Encode(&Allpost)
+}
+func addComment(w http.ResponseWriter, r *http.Request) {
+	val := r.URL.Path
+	var post postData
+	commentId, err := strconv.Atoi(strings.Split(val, "/")[2])
+	if err != nil {
+		log.Fatal("Invalid string conversion")
+		return
+	}
+	err = json.NewDecoder(r.Body).Decode(&post)
+	if err != nil {
+		http.Error(w, "Invali json type", http.StatusBadRequest)
+		return
+	}
+	for i, postitr := range Allpost {
+		if i == commentId {
+			postid := postitr[len(postitr)-1].PostId
+			Allpost[i] = append(Allpost[i], postData{
+				Id:     postitr[i].Id,
+				PostId: postid + 1,
+				Text:   post.Text,
+			})
+		}
+
+	}
+	w.WriteHeader(http.StatusAccepted)
+	json.NewEncoder(w).Encode(&Allpost)
+}
+
+func fetchPost(w http.ResponseWriter, r *http.Request) {
+	idstring := r.URL.Path
+	idval, err := strconv.Atoi(strings.Split(idstring, "/")[2])
+	if err != nil {
+		http.Error(w, "invalid get id", http.StatusBadRequest)
+	}
+	if r.Method != http.MethodGet {
+		http.Error(w, "ONLY GET METHOD ALLOWED", http.StatusMethodNotAllowed)
+		return
+	}
+	for i, data := range Allpost {
+		if i == idval {
+			fmt.Println("***", data)
+			w.WriteHeader(http.StatusAccepted)
+			json.NewEncoder(w).Encode(data)
+			break
+		}
+	}
+}
